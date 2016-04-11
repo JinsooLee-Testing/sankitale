cb2ec2e6cea2d0448d764fbf9e24ee1944c64695
lee Jinsoo <tmf2676@gmail.com>
2016-04-11 22:03:57 +0900

    no message

--- Lab07/RotateRandomAxis/main.cpp
+++ Lab07/RotateRandomAxis/main.cpp
@@ -19,183 +19,182 @@ class ESCListener : public FrameListener {
 
 public:
 	ESCListener(OIS::Keyboard *keyboard) : mKeyboard(keyboard) {}
-  bool frameStarted(const FrameEvent &evt)
-  {
-    mKeyboard->capture();
-	return !mKeyboard->isKeyDown(OIS::KC_ESCAPE);
-  }
+	bool frameStarted(const FrameEvent &evt)
+	{
+		mKeyboard->capture();
+		return !mKeyboard->isKeyDown(OIS::KC_ESCAPE);
+	}
 };
 
 
 class MainListener : public FrameListener {
-  OIS::Keyboard *mKeyboard;
-  Root* mRoot;
-  SceneNode *mProfessorNode,*mNinjaNode;
+	OIS::Keyboard *mKeyboard;
+	Root* mRoot;
+	SceneNode *mProfessorNode, *mNinjaNode;
 
 public:
-  MainListener(Root* root, OIS::Keyboard *keyboard) : mKeyboard(keyboard), mRoot(root) 
-  {
-    mProfessorNode = mRoot->getSceneManager("main")->getSceneNode("Professor");
-    mNinjaNode = mRoot->getSceneManager("main")->getSceneNode("Fish");
-  }
+	MainListener(Root* root, OIS::Keyboard *keyboard) : mKeyboard(keyboard), mRoot(root)
+	{
+		mProfessorNode = mRoot->getSceneManager("main")->getSceneNode("Professor");
+		mNinjaNode = mRoot->getSceneManager("main")->getSceneNode("Fish");
+	}
 
-  bool frameStarted(const FrameEvent &evt)
-  {
-    // Fill Here ----------------------------------------------
-	  static int i = 0;
-	  static float yAxis =1.0f;
-	  static float professorVelocity =200.0f; 
-	  Vector3 axis(0.0f, yAxis, 0.0f);
-	  static float roatationDegree = 0.0f;
-	  roatationDegree++;
-	  Vector3 rot(cos(i * 10) * 30.0f, 0.0f, sin(i * 10) * 30.0f);
-	  axis.normalise();
-	  if (mProfessorNode->getPosition().z < -250.0f || mProfessorNode->getPosition().z >250.0f){
-		  professorVelocity *= -1;
-		  yAxis *= -1;
-		  mProfessorNode->yaw(Degree(180.0f));
+	bool frameStarted(const FrameEvent &evt)
+	{
+		// Fill Here ----------------------------------------------
+		static float yAxis = 1.0f;
+		static float professorVelocity = 200.0f;
+		Vector3 axis(0.0f, yAxis, 0.0f);
+		static float roatationDegree = 0.0f;
+		roatationDegree++;
+		Vector3 rot(cos(i * 10) * 30.0f, 0.0f, sin(i * 10) * 30.0f);
+		axis.normalise();
+		if (mProfessorNode->getPosition().z < -250.0f || mProfessorNode->getPosition().z >250.0f) {
+			professorVelocity *= -1;
+			yAxis *= -1;
+			mProfessorNode->yaw(Degree(180.0f));
 
 
-	  }
+		}
 
-	  mProfessorNode->translate(px, py, professorVelocity * evt.timeSinceLastFrame);
-	  mNinjaNode->setPosition(cos(roatationDegree  *(3.141592 / 180.0f)) * 100.0f, 1.0f, sin(-roatationDegree * (3.141592 / 180.0f)) * 100.0f);
-	  mNinjaNode->setScale(8.0f, 8.0f, 8.0f);
-	  
-	  mNinjaNode->rotate(axis, Degree(0.5f));
+		mProfessorNode->translate(px, py, professorVelocity * evt.timeSinceLastFrame);
+		mNinjaNode->setPosition(cos(roatationDegree  *(PI / 180.0f)) * 100.0f, 1.0f, sin(-roatationDegree * (PI / 180.0f)) * 100.0f);
+		mNinjaNode->setScale(8.0f, 8.0f, 8.0f);
 
-    //-----------------------------------------------------------
+		mNinjaNode->rotate(axis, Degree(0.5f));
 
-    return true;
-  }
+		//-----------------------------------------------------------
+
+		return true;
+	}
 
 };
 
 class LectureApp {
 
-  Root* mRoot;
-  RenderWindow* mWindow;
-  SceneManager* mSceneMgr;
-  Camera* mCamera;
-  Viewport* mViewport;
-  OIS::Keyboard* mKeyboard;
-  OIS::InputManager *mInputManager;
+	Root* mRoot;
+	RenderWindow* mWindow;
+	SceneManager* mSceneMgr;
+	Camera* mCamera;
+	Viewport* mViewport;
+	OIS::Keyboard* mKeyboard;
+	OIS::InputManager *mInputManager;
 
-  MainListener* mMainListener;
-  ESCListener* mESCListener;
+	MainListener* mMainListener;
+	ESCListener* mESCListener;
 
 public:
 
-  LectureApp() {}
+	LectureApp() {}
 
-  ~LectureApp() {}
+	~LectureApp() {}
 
-  void go(void)
-  {
-    // OGRE의 메인 루트 오브젝트 생성
+	void go(void)
+	{
+		// OGRE의 메인 루트 오브젝트 생성
 #if !defined(_DEBUG)
-    mRoot = new Root("plugins.cfg", "ogre.cfg", "ogre.log");
+		mRoot = new Root("plugins.cfg", "ogre.cfg", "ogre.log");
 #else
-    mRoot = new Root("plugins_d.cfg", "ogre.cfg", "ogre.log");
+		mRoot = new Root("plugins_d.cfg", "ogre.cfg", "ogre.log");
 #endif
 
 
-    // 초기 시작의 컨피규레이션 설정 - ogre.cfg 이용
-    if (!mRoot->restoreConfig()) {
-      if (!mRoot->showConfigDialog()) return;
-    }
+		// 초기 시작의 컨피규레이션 설정 - ogre.cfg 이용
+		if (!mRoot->restoreConfig()) {
+			if (!mRoot->showConfigDialog()) return;
+		}
 
-    mWindow = mRoot->initialise(true, "Rotate on Random Axis : Copyleft by Dae-Hyun Lee");
-
-
-    // ESC key를 눌렀을 경우, 오우거 메인 렌더링 루프의 탈출을 처리
-    size_t windowHnd = 0;
-    std::ostringstream windowHndStr;
-    OIS::ParamList pl;
-    mWindow->getCustomAttribute("WINDOW", &windowHnd);
-    windowHndStr << windowHnd;
-    pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
-    mInputManager = OIS::InputManager::createInputSystem(pl);
-    mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, false));
+		mWindow = mRoot->initialise(true, "Rotate on Random Axis : Copyleft by Dae-Hyun Lee");
 
 
-    mSceneMgr = mRoot->createSceneManager(ST_GENERIC, "main");
-    mCamera = mSceneMgr->createCamera("main");
+		// ESC key를 눌렀을 경우, 오우거 메인 렌더링 루프의 탈출을 처리
+		size_t windowHnd = 0;
+		std::ostringstream windowHndStr;
+		OIS::ParamList pl;
+		mWindow->getCustomAttribute("WINDOW", &windowHnd);
+		windowHndStr << windowHnd;
+		pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+		mInputManager = OIS::InputManager::createInputSystem(pl);
+		mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, false));
 
 
-    mCamera->setPosition(0.0f, 100.0f, 700.0f);
-    mCamera->lookAt(0.0f, 100.0f, 0.0f);
-
-    mCamera->setNearClipDistance(5.0f);
-
-    mViewport = mWindow->addViewport(mCamera);
-    mViewport->setBackgroundColour(ColourValue(0.0f,0.0f,0.5f));
-    mCamera->setAspectRatio(Real(mViewport->getActualWidth()) / Real(mViewport->getActualHeight()));
+		mSceneMgr = mRoot->createSceneManager(ST_GENERIC, "main");
+		mCamera = mSceneMgr->createCamera("main");
 
 
-    ResourceGroupManager::getSingleton().addResourceLocation("resource.zip", "Zip");
-    ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+		mCamera->setPosition(0.0f, 100.0f, 700.0f);
+		mCamera->lookAt(0.0f, 100.0f, 0.0f);
 
-    mSceneMgr->setAmbientLight(ColourValue(1.0f, 1.0f, 1.0f));
+		mCamera->setNearClipDistance(5.0f);
 
-    // 좌표축 표시
-    Ogre::Entity* mAxesEntity = mSceneMgr->createEntity("Axes", "axes.mesh");
-    mSceneMgr->getRootSceneNode()->createChildSceneNode("AxesNode",Ogre::Vector3(0,0,0))->attachObject(mAxesEntity);
-    mSceneMgr->getSceneNode("AxesNode")->setScale(5, 5, 5);
-
-    _drawGridPlane();
+		mViewport = mWindow->addViewport(mCamera);
+		mViewport->setBackgroundColour(ColourValue(0.0f, 0.0f, 0.5f));
+		mCamera->setAspectRatio(Real(mViewport->getActualWidth()) / Real(mViewport->getActualHeight()));
 
 
-    Entity* entity1 = mSceneMgr->createEntity("Professor", "DustinBody.mesh");
-    SceneNode* node1 = mSceneMgr->getRootSceneNode()->createChildSceneNode("Professor", Vector3(px, py, pz));
-    node1->attachObject(entity1);
+		ResourceGroupManager::getSingleton().addResourceLocation("resource.zip", "Zip");
+		ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
-    Entity* entity2 = mSceneMgr->createEntity("Fish", "fish.mesh");
-    SceneNode* node2 = node1->createChildSceneNode("Fish", Vector3(100.0f, 0.0f, 0.0f));
-    node2->attachObject(entity2);
+		mSceneMgr->setAmbientLight(ColourValue(1.0f, 1.0f, 1.0f));
 
-    mESCListener =new ESCListener(mKeyboard);
-    mRoot->addFrameListener(mESCListener);
+		// 좌표축 표시
+		Ogre::Entity* mAxesEntity = mSceneMgr->createEntity("Axes", "axes.mesh");
+		mSceneMgr->getRootSceneNode()->createChildSceneNode("AxesNode", Ogre::Vector3(0, 0, 0))->attachObject(mAxesEntity);
+		mSceneMgr->getSceneNode("AxesNode")->setScale(5, 5, 5);
 
-    mMainListener = new MainListener(mRoot, mKeyboard);
-    mRoot->addFrameListener(mMainListener);
+		_drawGridPlane();
 
 
-    mRoot->startRendering();
+		Entity* entity1 = mSceneMgr->createEntity("Professor", "DustinBody.mesh");
+		SceneNode* node1 = mSceneMgr->getRootSceneNode()->createChildSceneNode("Professor", Vector3(px, py, pz));
+		node1->attachObject(entity1);
 
-    mInputManager->destroyInputObject(mKeyboard);
-    OIS::InputManager::destroyInputSystem(mInputManager);
+		Entity* entity2 = mSceneMgr->createEntity("Fish", "fish.mesh");
+		SceneNode* node2 = node1->createChildSceneNode("Fish", Vector3(100.0f, 0.0f, 0.0f));
+		node2->attachObject(entity2);
 
-    delete mRoot;
-  }
+		mESCListener = new ESCListener(mKeyboard);
+		mRoot->addFrameListener(mESCListener);
+
+		mMainListener = new MainListener(mRoot, mKeyboard);
+		mRoot->addFrameListener(mMainListener);
+
+
+		mRoot->startRendering();
+
+		mInputManager->destroyInputObject(mKeyboard);
+		OIS::InputManager::destroyInputSystem(mInputManager);
+
+		delete mRoot;
+	}
 
 private:
-  void _drawGridPlane(void)
-  {
-    Ogre::ManualObject* gridPlane =  mSceneMgr->createManualObject("GridPlane"); 
-    Ogre::SceneNode* gridPlaneNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("GridPlaneNode"); 
+	void _drawGridPlane(void)
+	{
+		Ogre::ManualObject* gridPlane = mSceneMgr->createManualObject("GridPlane");
+		Ogre::SceneNode* gridPlaneNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("GridPlaneNode");
 
-    Ogre::MaterialPtr gridPlaneMaterial = Ogre::MaterialManager::getSingleton().create("GridPlanMaterial","General"); 
-    gridPlaneMaterial->setReceiveShadows(false); 
-    gridPlaneMaterial->getTechnique(0)->setLightingEnabled(true); 
-    gridPlaneMaterial->getTechnique(0)->getPass(0)->setDiffuse(1,1,1,0); 
-    gridPlaneMaterial->getTechnique(0)->getPass(0)->setAmbient(1,1,1); 
-    gridPlaneMaterial->getTechnique(0)->getPass(0)->setSelfIllumination(1,1,1); 
+		Ogre::MaterialPtr gridPlaneMaterial = Ogre::MaterialManager::getSingleton().create("GridPlanMaterial", "General");
+		gridPlaneMaterial->setReceiveShadows(false);
+		gridPlaneMaterial->getTechnique(0)->setLightingEnabled(true);
+		gridPlaneMaterial->getTechnique(0)->getPass(0)->setDiffuse(1, 1, 1, 0);
+		gridPlaneMaterial->getTechnique(0)->getPass(0)->setAmbient(1, 1, 1);
+		gridPlaneMaterial->getTechnique(0)->getPass(0)->setSelfIllumination(1, 1, 1);
 
-    gridPlane->begin("GridPlaneMaterial", Ogre::RenderOperation::OT_LINE_LIST); 
-    for(int i=0; i<21; i++)
-    {
-      gridPlane->position(-500.0f, 0.0f, 500.0f-i*50);
-      gridPlane->position(500.0f, 0.0f, 500.0f-i*50);
+		gridPlane->begin("GridPlaneMaterial", Ogre::RenderOperation::OT_LINE_LIST);
+		for (int i = 0; i<21; i++)
+		{
+			gridPlane->position(-500.0f, 0.0f, 500.0f - i * 50);
+			gridPlane->position(500.0f, 0.0f, 500.0f - i * 50);
 
-      gridPlane->position(-500.f+i*50, 0.f, 500.0f);
-      gridPlane->position(-500.f+i*50, 0.f, -500.f);
-    }
+			gridPlane->position(-500.f + i * 50, 0.f, 500.0f);
+			gridPlane->position(-500.f + i * 50, 0.f, -500.f);
+		}
 
-    gridPlane->end(); 
+		gridPlane->end();
 
-    gridPlaneNode->attachObject(gridPlane);
-  }
+		gridPlaneNode->attachObject(gridPlane);
+	}
 };
 
 
@@ -204,28 +203,29 @@ extern "C" {
 #endif
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-  INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+	INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
 #else
-  int main(int argc, char *argv[])
+	int main(int argc, char *argv[])
 #endif
-  {
-    LectureApp app;
+	{
+		LectureApp app;
 
-    try {
+		try {
 
-      app.go();
+			app.go();
 
-    } catch( Ogre::Exception& e ) {
+		}
+		catch (Ogre::Exception& e) {
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-      MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+			MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
 #else
-      std::cerr << "An exception has occured: " <<
-        e.getFullDescription().c_str() << std::endl;
+			std::cerr << "An exception has occured: " <<
+				e.getFullDescription().c_str() << std::endl;
 #endif
-    }
+		}
 
-    return 0;
-  }
+		return 0;
+	}
 
 #ifdef __cplusplus
 }
