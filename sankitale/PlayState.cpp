59ba303a7770a6fe80ac26eed35715e2b2e10246
lee Jinsoo <tmf2676@gmail.com>
2016-06-19 04:19:25 +0900

    키보드로 카메라 회전 작업 - 왼쪽 오른쪽 화살표로 카메라 회전함

--- sankitale/PlayState.cpp
+++ sankitale/PlayState.cpp
@@ -9,12 +9,8 @@ PlayState PlayState::mPlayState;
 
 void PlayState::enter(void)
 {
-	/*mStatusOverlay = OverlayManager::getSingleton().getByName("Overlay/Status");
-	mHeartOverlay = OverlayManager::getSingleton().getByName("Overlay/SaveHeart");
-	mStatusMsg = OverlayManager::getSingleton().getOverlayElement("IDMsg");
-	mStatusHeartState = SAVE;
+	mCharacterRotateState = NORMAL;
 	mContinue = true;
-	mStatusState = PLAY;*/
 	mRoot = Root::getSingletonPtr();
 	mRoot->getAutoCreatedWindow()->resetStatistics();
 
@@ -22,50 +18,37 @@ void PlayState::enter(void)
 	mCamera = mSceneMgr->getCamera("main");
 	mCamera->setPosition(Ogre::Vector3::ZERO);
 
-	_drawGridPlane();
+	//_drawGridPlane();
 	_setLights();
 	_drawGroundPlane();
 
 	mInformationOverlay = OverlayManager::getSingleton().getByName("Overlay/Information");
 	mInformationOverlay->show();
 
-	
-	mCharacterRoot = mSceneMgr->getRootSceneNode()->createChildSceneNode("PlayerRoot");
-	mCharacterYaw = mCharacterRoot->createChildSceneNode("PlayerYaw");
-	//mCharacterYaw->yaw(Degree(180));
-	
-	mCharacterYaw->setScale(Vector3(10.0f, 10.0f, 10.0f));
+	mCharacterRoot = mSceneMgr->getRootSceneNode()->createChildSceneNode("ProfessorRoot");
+	mCharacterYaw = mCharacterRoot->createChildSceneNode("ProfessorYaw");
+
+	mCharacterDirection = Ogre::Vector3::ZERO;
+
 	mCameraYaw = mCharacterRoot->createChildSceneNode("CameraYaw", Vector3(0.0f, 120.0f, 0.0f));
 	mCameraPitch = mCameraYaw->createChildSceneNode("CameraPitch");
 	mCameraHolder = mCameraPitch->createChildSceneNode("CameraHolder", Vector3(0.0f, 80.0f, 500.0f));
 
-	mCharacterEntity = mSceneMgr->createEntity("Player", "fish.mesh");
+	mCharacterEntity = mSceneMgr->createEntity("Professor", "DustinBody.mesh");
 	mCharacterYaw->attachObject(mCharacterEntity);
-	//mCharacterYaw->setScale(1000.0f, 1000.0f, 1000.0f);
 	mCharacterEntity->setCastShadows(true);
 
 	mCameraHolder->attachObject(mCamera);
 	mCamera->lookAt(mCameraYaw->getPosition());
 
-	mCharacterDirection = Ogre::Vector3::ZERO;
-
-
-
-	mSavePointRoot = mSceneMgr->getRootSceneNode()->createChildSceneNode("ProfessorRoot");
-
-	mSavePointEntity = mSceneMgr->createEntity("Professor", "DustinBody.mesh");
-	mSavePointRoot->attachObject(mSavePointEntity);
-	mSavePointRoot->setPosition(Vector3(150.f, 0.0f, 150.f));
-	mEnemyAnimState = mSavePointEntity->getAnimationState("Lift");
-	mEnemyAnimState->setLoop(true);
-	mEnemyAnimState->setEnabled(true);
-
+	mRunAnimState = mCharacterEntity->getAnimationState("Run");
+	mRunAnimState->setLoop(true);
+	mRunAnimState->setEnabled(true);
 }
 
 void PlayState::exit(void)
 {
 	// Fill Here -----------------------------
-
 	mSceneMgr->clearScene();
 	mInformationOverlay->hide();
 	// ---------------------------------------
@@ -73,46 +56,33 @@ void PlayState::exit(void)
 
 void PlayState::pause(void)
 {
-	//_drawStatusPlane();
-	mHeartOverlay->show();
-	//if (mStatusHeartState == SAVE)	{ mBackHeartOverlay->show();   }
-	//else if (mStatusHeartState == BACK)	{ mSaveHeartOverlay->show(); }
-	mStatusOverlay->show();
-	mStatusMsg->show();
-	mEnemyAnimState->setLoop(false);
-	mEnemyAnimState->setEnabled(false);
+	//	mAnimationState = mCharacterEntity->getA
+	//	mAnimationState = mCharacterEntity->getAnimationState("Run");
+	mRunAnimState->setLoop(false);
+	mRunAnimState->setEnabled(false);
 }
 
 void PlayState::resume(void)
 {
-	mHeartOverlay->hide();
-	/*mBackHeartOverlay->hide();  
-	mSaveHeartOverlay->hide(); */
-	mStatusOverlay->hide();
-	mStatusMsg->hide();
-	mEnemyAnimState->setLoop(true);
-	mEnemyAnimState->setEnabled(true);
+	//	mAnimationState = mCharacterEntity->Anima`
+	mRunAnimState->setLoop(true);
+	mRunAnimState->setEnabled(true);
 }
 
 bool PlayState::frameStarted(GameManager* game, const FrameEvent& evt)
 {
-	mInformationOverlay =
-		OverlayManager::getSingleton().getByName("Overlay/Information");
-	mInformationOverlay->show();
-
-	/*mAnimationState->addTime(evt.timeSinceLastFrame);*/
-	mEnemyAnimState->addTime(evt.timeSinceLastFrame);
+	mRunAnimState->addTime(evt.timeSinceLastFrame);
 	static Vector3 offsetCamera = Vector3::ZERO;
 	static const float cameraDragSpeed = 100.f;
-
+	camerarotate();
 	// Fill Here -------------------------------------------------------------------
 	if (mCharacterDirection != Vector3::ZERO)
 	{
-		/*if (!mRunState->getEnabled())
-		{
-			mRunState->setEnabled(true);
-			mIdleState->setEnabled(false);
-		}*/
+		//if (!mRunState->getEnabled())
+		//{
+		//	mRunState->setEnabled(true);
+		//	mIdleState->setEnabled(false);
+		//}
 		//mRunState->addTime(evt.timeSinceLastFrame);
 
 		static const float mCharacterSpeed = 400.f;
@@ -133,11 +103,11 @@ bool PlayState::frameStarted(GameManager* game, const FrameEvent& evt)
 	}
 	else
 	{
-		/*if (!mIdleState->getEnabled())
-		{
-			mIdleState->setEnabled(true);
-			mRunState->setEnabled(false);
-		}*/
+		//if (!mIdleState->getEnabled())
+		//{
+		//	mIdleState->setEnabled(true);
+		//	mRunState->setEnabled(false);
+		//}
 		//mIdleState->addTime(evt.timeSinceLastFrame);
 
 		if (offsetCamera.length() > 0)
@@ -156,148 +126,60 @@ bool PlayState::frameStarted(GameManager* game, const FrameEvent& evt)
 			}
 		}
 	}
-
-	//if (WALK == mPlayerState)
-	//{
-	//	mAnimationState->setEnabled(false);
-	//	//mAnimationState = mCharacterEntity->getAnimationState("Walk");
-	//	mAnimationState->setLoop(true);
-	//	mAnimationState->setEnabled(true);
-	//}
-	//else if (RUN == mPlayerState)
-	//{
-	//	mAnimationState->setEnabled(false);
-	//	//mAnimationState = mCharacterEntity->getAnimationState("Run");
-	//	mAnimationState->setLoop(true);
-	//	mAnimationState->setEnabled(true);
-	//}
-	//else if (IDLE == mPlayerState)
-	//{
-	//	mAnimationState->setEnabled(false);
-	//	mAnimationState = mCharacterEntity->getAnimationState("Idle");
-	//	mAnimationState->setLoop(true);
-	//	mAnimationState->setEnabled(true);
-	//}
 	return mContinue;
 }
 
 bool PlayState::frameEnded(GameManager* game, const FrameEvent& evt)
 {
-//#if 0
+#if 0
 	static Ogre::DisplayString currFps = L"현재 FPS: ";
 	static Ogre::DisplayString avgFps = L"평균 FPS: ";
 	static Ogre::DisplayString bestFps = L"최고 FPS: ";
 	static Ogre::DisplayString worstFps = L"최저 FPS: ";
-	static Ogre::DisplayString position = L"Position: ";
 	OverlayElement* guiAvg = OverlayManager::getSingleton().getOverlayElement("AverageFps");
 	OverlayElement* guiCurr = OverlayManager::getSingleton().getOverlayElement("CurrFps");
 	OverlayElement* guiBest = OverlayManager::getSingleton().getOverlayElement("BestFps");
 	OverlayElement* guiWorst = OverlayManager::getSingleton().getOverlayElement("WorstFps");
-	OverlayElement* guiPos = OverlayManager::getSingleton().getOverlayElement("PlayerPos");
 	const RenderTarget::FrameStats& stats = mRoot->getAutoCreatedWindow()->getStatistics();
 	guiAvg->setCaption(avgFps + StringConverter::toString(stats.avgFPS));
 	guiCurr->setCaption(currFps + StringConverter::toString(stats.lastFPS));
 	guiBest->setCaption(bestFps + StringConverter::toString(stats.bestFPS));
 	guiWorst->setCaption(worstFps + StringConverter::toString(stats.worstFPS));
-	guiPos->setCaption(position + StringConverter::toString(mCharacterRoot->getPosition()));
-//#endif
+#endif
 	return mContinue;
 }
 
 
+bool PlayState::keyReleased(GameManager* game, const OIS::KeyEvent &e)
+{
+	switch (e.key)
+	{
+	//case OIS::KC_W: case OIS::KC_UP:    mCharacterDirection.z -= -1.f; 
+	//case OIS::KC_S: case OIS::KC_DOWN:  mCharacterDirection.z -= 1.f; break;
+	case OIS::KC_A: case OIS::KC_LEFT: mCharacterRotateState = NORMAL;  break;
+	//case OIS::KC_A: case OIS::KC_LEFT:  mCharacterDirection.x -= -1.f; break;
+	case OIS::KC_D: case OIS::KC_RIGHT:  mCharacterRotateState = NORMAL; break;
+	case OIS::KC_ESCAPE: mContinue = false; break;
+	}
+	return true;
+}
+
 bool PlayState::keyPressed(GameManager* game, const OIS::KeyEvent &e)
 {
 	// Fill Here -------------------------------------------
-	if (PLAY == mStatusState)
+	switch (e.key)
 	{
-		switch (e.key)
-		{
-		case OIS::KC_W: case OIS::KC_UP:    mCharacterDirection.z += -1.f; break;
-		case OIS::KC_S: case OIS::KC_DOWN:  mCharacterDirection.z += 1.f; break;
-		case OIS::KC_A: case OIS::KC_LEFT:  mCharacterDirection.x += -1.f; break;
-		case OIS::KC_D: case OIS::KC_RIGHT: mCharacterDirection.x += 1.f; break;
-		//case OIS::KC_UP:case OIS::KC_W:
-		//{mPlayerState = WALK;
-		//mCharacterDirection.z += -1.f;	}break;
-		//case OIS::KC_DOWN:case OIS::KC_S:
-		//{mPlayerState = WALK; mCharacterDirection.z += 1.f;	}break;
-		//case OIS::KC_LEFT: case OIS::KC_A:
-		//{mPlayerState = WALK; mCharacterDirection.x += -1.f; }break;
-		//case OIS::KC_RIGHT: case OIS::KC_D:
-		//{mPlayerState = WALK; mCharacterDirection.x += 1.f; }break;
-		//case OIS::KC_LSHIFT:
-		//	//if (WALK == mPlayerState)
-		//{mPlayerState = RUN; }break;
-		case OIS::KC_ESCAPE:
-			mContinue = false;
-			//game->changeState(TitleState::getInstance());
-			break;
-		case OIS::KC_X:
-				pause();
-				mStatusState = STATUSOVERLAY;
-
-			break;
-
-		}
-	}
-	else if (STATUSOVERLAY == mStatusState)
-	{
-		switch (e.key)
-		{
-		case OIS::KC_LEFT:
-		{
-			if (mStatusHeartState == SAVE)	{ 
-				
-				mStatusHeartState = BACK; }
-			else if (mStatusHeartState == BACK)	{ mStatusHeartState = SAVE; }
-
-		}
-		break;
-		case OIS::KC_RIGHT:
-		{
-			if (mStatusHeartState == SAVE)	{ mStatusHeartState = BACK; }
-			else if (mStatusHeartState == BACK)	{ mStatusHeartState = SAVE; }
-		}
-		break;
-		case OIS::KC_X:
-			resume();
-			mStatusState = PLAY;
-			break;
-		case OIS::KC_ESCAPE:
-			mContinue = false;
-			break;
-		}
-		
+	//case OIS::KC_W: case OIS::KC_UP:    mCharacterDirection.z += -1.f; break;
+	//case OIS::KC_S: case OIS::KC_DOWN:  mCharacterDirection.z += 1.f; break;
+	case OIS::KC_A: case OIS::KC_LEFT: mCharacterRotateState = LEFT_ROTATE; break;
+	//case OIS::KC_A: case OIS::KC_LEFT:  mCharacterDirection.x += -1.f; break;
+	case OIS::KC_D: case OIS::KC_RIGHT: mCharacterRotateState = RIGHT_ROTATE; break;
+	case OIS::KC_ESCAPE: mContinue = false; break;
 	}
 	// -----------------------------------------------------
 	return true;
 }
 
-bool PlayState::keyReleased(GameManager* game, const OIS::KeyEvent &e)
-{
-	if (PLAY == mStatusState){
-		switch (e.key)
-		{
-		case OIS::KC_W: case OIS::KC_UP:    mCharacterDirection.z -= -1.f; break;
-		case OIS::KC_S: case OIS::KC_DOWN:  mCharacterDirection.z -= 1.f; break;
-		case OIS::KC_A: case OIS::KC_LEFT:  mCharacterDirection.x -= -1.f; break;
-		case OIS::KC_D: case OIS::KC_RIGHT: mCharacterDirection.x -= 1.f; break;
-		}
-	}
-	//case OIS::KC_UP: case OIS::KC_W:
-	//	if (WALK == mPlayerState || RUN == mPlayerState){ mPlayerState = IDLE; mCharacterDirection.z -= -1.f; }break;
-	//case OIS::KC_DOWN:case OIS::KC_S:
-	//	if (WALK == mPlayerState || RUN == mPlayerState){ mPlayerState = IDLE; mCharacterDirection.z -= 1.f; }break;
-	//case OIS::KC_LEFT: case OIS::KC_A:
-	//	if (WALK == mPlayerState || RUN == mPlayerState){ mPlayerState = IDLE; mCharacterDirection.x -= -1.f; }break;
-	//case OIS::KC_RIGHT: case OIS::KC_D:
-	//	if (WALK == mPlayerState || RUN == mPlayerState){ mPlayerState = IDLE; mCharacterDirection.x -= 1.f; }break;
-	//case OIS::KC_LSHIFT:
-	//	//if (RUN == mPlayerState)
-	//{ mPlayerState = WALK; }break;
-	//}
-	return true;
-}
 bool PlayState::mousePressed(GameManager* game, const OIS::MouseEvent &e, OIS::MouseButtonID id)
 {
 	return true;
@@ -311,36 +193,33 @@ bool PlayState::mouseReleased(GameManager* game, const OIS::MouseEvent &e, OIS::
 
 bool PlayState::mouseMoved(GameManager* game, const OIS::MouseEvent &e)
 {
-	if (PLAY == mStatusState)
-	{
-		mCameraYaw->yaw(Degree(-e.state.X.rel));
-		mCameraPitch->pitch(Degree(-e.state.Y.rel));
+	/*mCameraYaw->yaw(Degree(-e.state.X.rel));
+	mCameraPitch->pitch(Degree(-e.state.Y.rel));
 
-		mCameraHolder->translate(Ogre::Vector3(0, 0, -e.state.Z.rel * 0.1f));
-	}
+	mCameraHolder->translate(Ogre::Vector3(0, 0, -e.state.Z.rel * 0.1f));*/
 	return true;
 }
 
-void PlayState::_drawStatusPlane(void)
+void PlayState::camerarotate()
 {
-	
-	
-	
-	/*Plane plane(Vector3::UNIT_Y, 0);
-	MeshManager::getSingleton().createPlane(
-		"Ground",
-		ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-		plane,
-		2000, 2000,
-		1, 1,
-		true, 1, 1, 1,
-		Vector3::NEGATIVE_UNIT_Z
-		);
-
-	Entity* groundEntity = mSceneMgr->createEntity("GroundPlane", "Ground");
-	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
-	groundEntity->setMaterialName("ruin_first");
-	groundEntity->setCastShadows(false);*/
+	if (LEFT_ROTATE == mCharacterRotateState)
+	{
+		mCameraYaw->rotate(Ogre::Vector3::UNIT_Y,
+			Ogre::Degree(-0.5f),
+			Ogre::Node::TS_PARENT);
+	}
+	else if (RIGHT_ROTATE == mCharacterRotateState)
+	{
+		mCameraYaw->rotate(Ogre::Vector3::UNIT_Y,
+			Ogre::Degree(0.5f),
+			Ogre::Node::TS_PARENT);
+	}
+	else if (NORMAL == mCharacterRotateState)
+	{
+		mCameraYaw->rotate(Ogre::Vector3::UNIT_Y,
+			Ogre::Degree(0.f),
+			Ogre::Node::TS_PARENT);
+	}
 }
 
 void PlayState::_setLights(void)
@@ -361,15 +240,15 @@ void PlayState::_drawGroundPlane(void)
 		"Ground",
 		ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
 		plane,
-		2000, 2000,
+		500, 500,
 		1, 1,
-		true, 1, 1, 1,
+		true, 1, 5, 5,
 		Vector3::NEGATIVE_UNIT_Z
 		);
 
 	Entity* groundEntity = mSceneMgr->createEntity("GroundPlane", "Ground");
 	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
-	groundEntity->setMaterialName("ruin_first");
+	groundEntity->setMaterialName("KPU_LOGO");
 	groundEntity->setCastShadows(false);
 }
 
@@ -404,4 +283,3 @@ void PlayState::_drawGridPlane(void)
 
 	gridPlaneNode->attachObject(gridPlane);
 }
-
--- sankitale/PlayState.h
+++ sankitale/PlayState.h
@@ -23,17 +23,17 @@ public:
 
 	static PlayState* getInstance() { return &mPlayState; }
 
-	void setAnimState(Ogre::AnimationState* anim) { mAnimationState = anim; }
-
+	void setAnimState(Ogre::AnimationState* anim) { mRunAnimState = anim; }
+	void camerarotate();
 private:
-	//const Ogre::FrameEvent& evt;
+
 	void _setLights(void);
 	void _drawGroundPlane(void);
 	void _drawGridPlane(void);
-	void _drawStatusPlane(void);
-	bool mContinue;
-	static PlayState mPlayState;
 
+
+	static PlayState mPlayState;
+	Ogre::Vector3 mCharacterDirection;
 	Ogre::Root *mRoot;
 	Ogre::RenderWindow* mWindow;
 	Ogre::SceneManager* mSceneMgr;
@@ -46,32 +46,14 @@ private:
 	Ogre::SceneNode* mCameraHolder;
 	Ogre::SceneNode* mCameraYaw;
 	Ogre::SceneNode* mCameraPitch;
-	
 
-	
-	Ogre::Vector3 mCharacterDirection;
 	Ogre::Entity* mCharacterEntity;
-	
-	
 
-	Ogre::AnimationState* mAnimationState;
-	Ogre::AnimationState* mEnemyAnimState;
+	Ogre::AnimationState* mRunAnimState;
+
 	Ogre::Overlay*           mInformationOverlay;
-	int mPlayerState;
-	int mCameraState;
-	int mStatusState;
-	int mStatusHeartState;
-	enum {RIGHT_ROTATION,LEFT_ROTATION,NORMAL};
-	enum  {IDLE,WALK,RUN};
-	enum{STATUSOVERLAY,PLAY};
-	enum{SAVE,BACK};
 
-	Ogre::SceneNode* mSavePointRoot;
-	Ogre::Entity* mSavePointEntity;
-
-
-	Ogre::Overlay* mStatusOverlay;
-	Ogre::Overlay* mHeartOverlay;
-	//Ogre::Overlay* mBackHeartOverlay;
-	Ogre::OverlayElement* mStatusMsg;
+	bool mContinue;
+	enum{LEFT_ROTATE,RIGHT_ROTATE,NORMAL};
+	short mCharacterRotateState;
 };
